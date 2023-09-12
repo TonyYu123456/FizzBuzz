0b7386b8899d281cde74de3c74484164578930b7
TonyYu123456 <shijun.yu@mail.utoronto.ca>
2023-09-11 22:34:26 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -11,7 +11,7 @@ class FizzBuzz {
             doFizzBuzz(i);
         }
     }
-//
+    //
     private static void doFizzBuzz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
